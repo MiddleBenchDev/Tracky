4cba5496214cec25aa407b6dbb0d563d8b78dbf0
MiddleBenchDev <middlebenchdev@gmail.com>
2025-10-02 22:30:26 +0530

    Db config and minor UI tweaks

--- app/navBar.tsx
+++ app/navBar.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { FaOctopusDeploy } from 'react-icons/fa'
 
@@ -7,6 +10,8 @@ const NavBar = () => {
         { label: 'Dashboard', href: '/' },
         { label: 'Issues', href: '/issues' }
     ]
+
+    const path = usePathname()
     return (
         <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
             <Link href='/'><FaOctopusDeploy size={40} /></Link>
@@ -15,7 +20,7 @@ const NavBar = () => {
                     links?.map((link, index) => (
                         <Link
                             key={index}
-                            className='text-zinc-500 hover:text-zinc-800 transition-colors'
+                            className={`${link?.href == path ? 'text-zinc-900' : 'text-zinc-500'} hover:text-zinc-800 transition-colors`}
                             href={link?.href}
                         >
                             {link?.label}
